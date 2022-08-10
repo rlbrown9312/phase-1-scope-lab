ea687a40d10514f2a9307096bbd66dbe6116a790
Richard <richard.brown0807@gmail.com>
2022-08-10 08:28:16 -0500

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+//SCOPE LAB
+
+var customerName = "bob";
+const leastFavoriteCustomer = "Jennifer";
+ 
+
+function upperCaseCustomerName() {
+customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+return bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+bestCustomer = "maybe bob";
+}
+
+function changeLeastFavoriteCustomer() {
+leastFavoriteCustomer = "Jasmine";
+}
+
